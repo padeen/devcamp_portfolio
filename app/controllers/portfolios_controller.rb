c1a193b8b821e37a92aa676606e9635037be996e
padeen <paulo.deen@gmail.com>
2017-08-13 21:26:15 +0200

    Add before action for @portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
 	layout 'portfolio'
 
 	def index
@@ -27,11 +28,9 @@ class PortfoliosController < ApplicationController
   end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
   def update
-		@portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -43,11 +42,9 @@ class PortfoliosController < ApplicationController
   end
 
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
   def destroy
-		@portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
 
     respond_to do |format|
@@ -63,5 +60,9 @@ class PortfoliosController < ApplicationController
 																			:body,
 																		 	technologies_attributes: [:name]
 																		 )
+	end
+
+	def set_portfolio_item
+		@portfolio_item = Portfolio.find(params[:id])
 	end
 end
